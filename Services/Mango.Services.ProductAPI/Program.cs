a90dd89a56ea15e8c3e2667eb78ac39064e48a2a
Gabriel Batista <gabriel_batista@outlook.com>
2021-11-09 15:45:26 -0300

    (feat) creating and registering DbContext

--- Services/Mango.Services.ProductAPI/Program.cs
+++ Services/Mango.Services.ProductAPI/Program.cs
@@ -1,4 +1,10 @@
+using Mango.Services.ProductAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Get the connection string value
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 
@@ -6,6 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registering the DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 

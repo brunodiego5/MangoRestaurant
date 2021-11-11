e5ebe87178ba5127cbe2f8ecb645508693aab105
Gabriel Batista <gabriel_batista@outlook.com>
2021-11-11 15:24:06 -0300

    (feat) registering the AutoMapper configurations as a singleton

--- Services/Mango.Services.ProductAPI/Program.cs
+++ Services/Mango.Services.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using Mango.Services.ProductAPI;
 using Mango.Services.ProductAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +14,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registering as singleton the AutoMapper configurations from the MappingConfig file
+IMapper mapper = MappingConfig.RegisterMaps()
+                              .CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Registering the DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));

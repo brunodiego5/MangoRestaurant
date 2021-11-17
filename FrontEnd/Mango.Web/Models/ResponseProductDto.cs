4e73608b863d1c916d751845ef15aa7a1074b7e0
Gabriel Batista <gabriel_batista@outlook.com>
2021-11-17 14:43:06 -0300

    (wip) preparing the front-end to consume the ProductAPI

---
+++ FrontEnd/Mango.Web/Models/ResponseProductDto.cs
@@ -0,0 +1,9 @@
+namespace Mango.Web.Models;
+
+public class ResponseProductDto
+{
+    public bool IsSuccess { get; set; } = true;
+    public object Result { get; set; }
+    public string DisplayMessage { get; set; } = string.Empty;
+    public List<string> ErrorMessages { get; set; }
+}
---
+++ FrontEnd/Mango.Web/StaticDetails.cs
@@ -0,0 +1,12 @@
+namespace Mango.Web;
+
+public static class StaticDetails
+{
+    public static string ProductAPIBase { get; set; }
+    public enum ApiType {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+}

d0d2178f2d1c3fa5ffdffd60cb09246532d57284
Gabriel Batista <gabriel_batista@outlook.com>
2021-11-17 11:04:18 -0300

    (fix) registering AddLogging service and moving dbContext closer to connection string var

--- Services/Mango.Services.ProductAPI/Program.cs
+++ Services/Mango.Services.ProductAPI/Program.cs
@@ -8,10 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Get the connection string value
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Registering the DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Add loggin services.
+builder.Services.AddLogging();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,9 +30,6 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Registering the IProductRepository
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-
-// Registering the DbContext
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 

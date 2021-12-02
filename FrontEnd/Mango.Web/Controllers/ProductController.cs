07fad17b1d6924e47193d5fb83825cba5d935aa3
Gabriel Batista <gabriel_batista@outlook.com>
2021-12-02 20:10:07 -0300

    (wip) starting the consumption of the GetAllProductsAsync

---
+++ FrontEnd/Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+using System.Text.Json;
+using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly IProductService _productService;
+    private readonly ILogger<ProductController> _logger;
+
+    public ProductController(IProductService productService, ILogger<ProductController> logger)
+    {
+        _productService = productService;
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> ProductIndex()
+    {
+        List<ProductDto> products = new();
+        var response = await _productService.GetAllProductsAsync<ResponseProductDto>();
+        if(response != null && response.IsSuccess)
+        {
+            products = JsonSerializer.Deserialize<List<ProductDto>>(response.Result.ToString());
+        }
+        
+        return View(products);
+    }
+}
